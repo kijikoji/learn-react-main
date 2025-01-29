8d19d52069c60e9d9a9aa0b8c486571a2341b6cb
kijikoji <102621826+kijikoji@users.noreply.github.com>
2025-01-29 11:58:08 -0600

    closes 01/18
    Custom Components - Parent/Child Components

--- 01. Static pages/18. Custom Components - Parent Child Components/index.jsx
+++ 01. Static pages/18. Custom Components - Parent Child Components/index.jsx
@@ -10,22 +10,48 @@ const root = createRoot(document.getElementById("root"))
  * the Page component where the `header` used to be.
  */
 
+/** Challenge: 
+ * Move the `main` element into its own component called "MainContent" 
+ * and render that component inside the Page component.
+ * 
+ * Do the same with the `footer` element, moving it into a new
+ * component called "Footer"
+*/
+
+function Header() {
+    return (
+        <header>
+            <img src="react-logo.png" width="40px" alt="React logo" />
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
 function Page() {
     return (
         <>
-            <header>
-                <img src="react-logo.png" width="40px" alt="React logo" />
-            </header>
-            <main>
-                <h1>Reason I am excited to learn React</h1>
-                <ol>
-                    <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                    <li>I am more likely to get a job as a front end developer if I know React</li>
-                </ol>
-            </main>
-            <footer>
-                <small>© 2024 Ziroll development. All rights reserved.</small>
-            </footer>
+            <Header />
+            <MainContent />
+            <Footer />
         </>
     )
 }
