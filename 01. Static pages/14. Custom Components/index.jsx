00ac7cc556a2504c04a3b7884a32779711da78cb
kijikoji <102621826+kijikoji@users.noreply.github.com>
2025-01-29 11:38:52 -0600

    closes 01/14
    Custom Components

--- 01. Static pages/14. Custom Components/index.jsx
+++ 01. Static pages/14. Custom Components/index.jsx
@@ -1,6 +1,13 @@
 import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 
+
+
+root.render(
+    <MyPage />
+
+)
+
 /**
  * Challenge (part 1):
  * Create a custom "Page" component
@@ -11,6 +18,18 @@ const root = createRoot(document.getElementById("root"))
  * Render the Page component.
  */
 
-root.render(
-    
-)
+function MyPage() {
+    return (
+        <main>
+            <h1>why im excited to be learning React!</h1>
+            <ul>
+                <li>to learn a popular language</li>
+                <li>to build a portfolio</li>
+                <li>to make projects</li>
+                <li>use my knowledge html css and javascript</li>
+            </ul>
+        </main>
+    )
+}
+
+
