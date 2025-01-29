792c4f0c7fce5926e1ea4f84a949eb1f4c5e0dec
kijikoji <102621826+kijikoji@users.noreply.github.com>
2025-01-29 11:27:12 -0600

    closes 01/07
    React.createElement()

--- 01. Static pages/07. React.createElement()/index.jsx
+++ 01. Static pages/07. React.createElement()/index.jsx
@@ -1,6 +1,11 @@
+import { createElement } from "react"
 import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
+const reactElement = createElement("h1", null, "Hello from createElement!")
+
+console.log(reactElement)
+
 root.render(
-    <h1>Hello from React!</h1>
+    reactElement
 )
