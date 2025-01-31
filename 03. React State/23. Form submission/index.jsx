357771154539ea74b20f575bc4e3ce07f5347db5
kijikoji <102621826+kijikoji@users.noreply.github.com>
2025-01-31 09:10:55 -0600

    closes 03/23

--- 03. React State/23. Form submission/index.jsx
+++ 03. React State/23. Form submission/index.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(event)
+    const formEl = event.currentTarget
+    console.log(formEl)
+    const formData = new FormData(formEl)
+    const email = formData.get("email")
+    // Gather the info from the form
+    // Submit it to a backend
+    formEl.reset()
+  }
+  
   return (
     <section>
       <h1>Signup form</h1>
-      <form>
+      <form onSubmit={handleSubmit} method="post">
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
         <br />
