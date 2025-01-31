d0919f29daa53e943d8e853eae028f6081ddd139
kijikoji <102621826+kijikoji@users.noreply.github.com>
2025-01-31 10:59:33 -0600

    closes 03/34

--- 03. React State/34. Conditional rendering practice - AndAnd/App.jsx
+++ 03. React State/34. Conditional rendering practice - AndAnd/App.jsx
@@ -5,12 +5,25 @@ export default function App() {
     
     /**
      * Challenge:
-     * Only display the <h1> below if there are unread messages
+     * If there are 0 unread messages, display a paragraph that says "You
+     * have no unread messages". (So, the logic will be the opposite of
+     * what we have for the h1)
      */
     
     return (
-        <div>
-            <h1>You have _ unread messages!</h1>
-        </div>
+        <>
+            <div>
+                {
+                    unreadMessages.length == 0 && 
+                    <h1>You have no unread messages</h1>
+                }
+            </div>
+            <div>
+                {
+                    unreadMessages.length > 0 && 
+                    <h1>You have {unreadMessages.length} unread messages!</h1>
+                }
+            </div>
+        </>
     )
 }
