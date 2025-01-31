f397d02caf3e9e1b9f343d12eced27363df91f09
kijikoji <102621826+kijikoji@users.noreply.github.com>
2025-01-30 19:08:14 -0600

    closes 03/06

--- 03. React State/06. Chef Claude - Map ingredients list/Main.jsx
+++ 03. React State/06. Chef Claude - Map ingredients list/Main.jsx
@@ -8,19 +8,46 @@ export default function Main() {
      * Note: We're doing things a weird way here. Don't worry,
      * we're building up to learning the right way 🙂
      */
+
+    const ingredientsList = ingredients.map(x => <li key={x}>{x}</li>)
+
+    /**
+     * Challenge:
+     * Add an `onSubmit` event listener on the form. Have the function
+     * simply console.log("Form submitted!") for now
+     */
+
+    function handleSubmit(event) {
+       event.preventDefault()
+       const formData = new FormData(event.currentTarget)
+       const newIngredient = formData.get("ingredient")
+       ingredients.push(newIngredient)
+       console.log(ingredients)
+   }
+
+     /**
+     * Challenge:
+     * Add the new ingredient to the array of ingredients. Also, add a 
+     * console.log(ingredients) after adding the ingredient, because 
+     * **warning**: you aren't going to see the page update!
+     * 
+     * Hint: this is a one-liner solution, so don't overthink it 😅
+     */
+
     
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
             <ul>
-                {/* Render ingredientsList here */}
+                {ingredientsList}
             </ul>
         </main>
     )
