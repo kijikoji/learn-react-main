d3aa8161d5a8020119d8be79863982d252d085cf
kijikoji <102621826+kijikoji@users.noreply.github.com>
2025-02-03 15:27:41 -0600

    closes 03/45

--- 03. React State/45. Dynamic styles/App.jsx
+++ 03. React State/45. Dynamic styles/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import padsData from "./pads"
 
-export default function App() {
+export default function App(props) {
     const [pads, setPads] = React.useState(padsData)
     
     /**
@@ -10,9 +10,13 @@ export default function App() {
      * If darkMode is true, set them to "#222222"
      * If darkMode is false, set them to "#cccccc"
      */
+
+    const style = {
+        backgroundColor: props.darkMode ? "#222222" : "#cccccc",
+    }
     
     const buttonElements = pads.map(pad => (
-        <button key={pad.id}></button>
+        <button key={pad.id} style={style}></button>
     ))
     
     return (
