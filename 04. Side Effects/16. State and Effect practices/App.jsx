4ec132432a033d88b919a4be316bdeeb12d07f70
kijikoji <cvillagr97@gmail.com>
2025-02-07 19:12:47 -0600

    closes 16

--- 04. Side Effects/16. State and Effect practices/App.jsx
+++ 04. Side Effects/16. State and Effect practices/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react"
 import WindowTracker from "./WindowTracker"
+
 
 export default function App() {
     /**
@@ -6,14 +8,15 @@ export default function App() {
      * 1. Create state called `show`, default to `true`
      * 2. When the button is clicked, toggle `show`
      * 3. Only display `<WindowTracker>` if `show` is `true`
-     */
-    
+    */
+   const [show, setShow] = useState(true)
+   
     return (
         <main className="container">
-            <button>
+            <button onClick={() => setShow(prevShow => !prevShow)}>
                 Toggle WindowTracker
             </button>
-            <WindowTracker />
+            {show ? <WindowTracker /> : null}
         </main>
     )
 }
