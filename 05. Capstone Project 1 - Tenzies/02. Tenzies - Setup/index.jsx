c8810ce3260fcea74810fd4a31682306b4651721
kijikoji <102621826+kijikoji@users.noreply.github.com>
2025-02-10 11:43:00 -0600

    closes 02 no css

--- 05. Capstone Project 1 - Tenzies/02. Tenzies - Setup/index.jsx
+++ 05. Capstone Project 1 - Tenzies/02. Tenzies - Setup/index.jsx
@@ -5,3 +5,9 @@
  * - In the App component, render a <main> element
  * - Style everything to look like the slide
  */
+import ReactDom from "react-dom/client"
+import App from "./App"
+
+ReactDom.createRoot(document.getElementById("root")).render(
+    <App />
+)
