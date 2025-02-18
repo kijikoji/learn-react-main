5baf3333c0630a1bf6759b06b1e96177f787d427
kijikoji <102621826+kijikoji@users.noreply.github.com>
2025-02-18 15:18:58 -0600

    closes 02

--- 06. Capstone Project #2 - Assembly Endgame/02. Assembly Endgame - Project Planning/App.jsx
+++ 06. Capstone Project #2 - Assembly Endgame/02. Assembly Endgame - Project Planning/App.jsx
@@ -7,15 +7,22 @@ import React from "react"
  * 
  * - What are the main containers of elements I need
  *   in this app?
- * 
+ * list of all the languages 
+ * list of all letters
+ * box to hold character of selected word
+ * button for new game
  * 
  * - What values will need to be saved in state vs.
  *   what values can be derived from the state?
+ * languages need a name, color, text color, and if its dead to be stored as a list of objects
+ * letters need name and if its been selected in state color can be derived
+ * bowxes can be derived from the selected word
+ * button state can be derived from the game state
  * 
  * 
  * - How will the user interact with the app? What
  *   events do I need to handle?
- * 
+ * though clicking the letter buttons
  * 
  */
 
